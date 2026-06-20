5ee71849e95585eb231f3ce7699b5e9ca276b1cb
ElShyro <yshiromt@gmail.com>
2026-06-19 19:38:28 -0500

    Corrección en módulo de ventas

--- Granja/Ventas.cs
+++ Granja/Ventas.cs
@@ -19,6 +19,8 @@ namespace Granja
             btnRegistrarVenta.Click += BtnRegistrarVenta_Click;
             txtCantidad.TextChanged += ActualizarTotal;
             txtPrecio.TextChanged += ActualizarTotal;
+
+            // Corrección realizada para práctica de control de versiones
         }
 
         private void ActualizarTotal(object sender, EventArgs e)
